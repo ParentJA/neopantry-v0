6b7b533a15330badf4a7c1a1cea16984d1b64e76
Jason Parent <ParentJ@advisory.com>
2016-08-06 21:08:08 -0400

    Updated requirements and settings.

--- settings/base.py
+++ settings/base.py
@@ -70,13 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'neopantry.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -102,8 +95,3 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DJANGO_WYSIWYG_FLAVOR = 'tinymce_advanced'
 
 LETTUCE_USE_TEST_DATABASE = True
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
---
+++ settings/production.py
@@ -0,0 +1,4 @@
+# Local imports...
+from .base import *
+
+__author__ = 'Jason Parent'
